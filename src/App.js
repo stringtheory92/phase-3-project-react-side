8aeaee27eb4e81fc02eb44b1ae4c6924f15e9858
Salman Mahmud <salman_mahmud@hotmail.com>
2022-11-14 18:17:58 -0500

    Component Hierarchy, routes, basic form MOST UP TO DATE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './Login';
-import {BrowserRouter as Router, Route, Routes,} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './Navbar';
 import HomeScreen from './HomeScreen';
 
 function App() {
-  isLoggedIn = localStorage.getItem("username") ? <HomeScreen /> : <Login />
+  const isLoggedIn = localStorage.getItem("username") ? <HomeScreen /> : <Login />
   return (
 
     <Router>
