b63fb9f5ec8ba2fd97aa6c9208860182e0c6501d
Adam Kabak <adamkabak@yahoo.com>
2022-11-15 12:05:53 -0500

    testing version flow

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import './App.css';
-import Login from './Login';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Navbar from './Navbar';
-import Trading from './Trading';
-import WatchList from './WatchList';
-import HomeScreen from './HomeScreen';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import Login from "./Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import Trading from "./Trading";
+import WatchList from "./WatchList";
+import HomeScreen from "./HomeScreen";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userState, setUserState] = useState({});
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [userState, setUserState] = useState({})
+  useEffect(() => {
+    const currentUser = localStorage.getItem("username");
 
-  useEffect(() =>{
-    const currentUser = localStorage.getItem("username")
-
-    console.log(currentUser)
-    if (currentUser){
-      setIsLoggedIn(true)
-      fetch(`http://localhost:9292/user/${currentUser}`)
-      .then(resp => resp.json())
-      .then(data => setUserState(data))
+    console.log(currentUser);
+    if (currentUser) {
+      setIsLoggedIn(true);
+      fetch(`http://localhost:9292/users/${currentUser}`)
+        .then((resp) => resp.json())
+        .then((data) => setUserState(data));
+    } else {
+      setIsLoggedIn(false);
     }
+  }, []);
 
-    else{
-      setIsLoggedIn(false)
-    }
-
-  }, [])
-
-  console.log(userState)
+  console.log(userState);
   return (
-
     <Router>
-      <Navbar/>
-        <Routes>
-          <Route exact path='/' element={isLoggedIn ? ( <HomeScreen />) : ( <Login replace to={"/login"}/>)}/>
-          <Route path='/trading' element={<Trading userState={userState}/>}/>
-          <Route path='/watchlist' element={<WatchList userState={userState}/>}/>
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            isLoggedIn ? <HomeScreen /> : <Login replace to={"/login"} />
+          }
+        />
+        <Route path="/trading" element={<Trading userState={userState} />} />
+        <Route
+          path="/watchlist"
+          element={<WatchList userState={userState} />}
+        />
+      </Routes>
     </Router>
     // <div className="App">
     //   {isLoggedIn}
--- src/Trading.js
+++ src/Trading.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-function Trading() {
-  return (
-    <div>Trading</div>
-  )
+function Trading({ userState }) {
+  // localStorage.setItem({key: value})
+
+  // console.log("userState: ", userState);
+  console.log("Local Storage User: ", localStorage.getItem("username"));
+
+  return <div>Trading</div>;
 }
 
-export default Trading
+export default Trading;
