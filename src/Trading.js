00040b7e33c4be5836d376607d4d39e858011fda
Adam Kabak <adamkabak@yahoo.com>
2022-11-15 18:09:46 -0500

    trading

--- src/Trading.js
+++ src/Trading.js
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from "react";
 
 function Trading({ userState }) {
-  const [userBalance, setUserBalance] = useState(0);
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+    balance: "",
+  });
   // localStorage.setItem({key: value})
 
+  const updateUser = ({ user_name: userName, password, balance }) => {
+    const newBalance = balance === user.balance ? balance : user.balance;
+
+    setUser({ ...user, balance: newBalance });
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:9292/users/${localStorage.getItem("username")}`)
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log("trading user: ", data);
+          // const newBalance = (balance === user.balance) ? balance : user.balance
+          // const { user_name: userName, password, balance } = data;
+          updateUser(data);
+        });
+    }
+  }, [user.balance]);
   // console.log("userState: ", userState);
-  console.log("Local Storage User: ", localStorage.getItem("username"));
+  console.log(
+    "(Trading) Local Storage User: ",
+    localStorage.getItem("username")
+  );
 
   return <div>Trading</div>;
 }
